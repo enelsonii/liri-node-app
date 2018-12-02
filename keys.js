677d630afdd1a5338ddf3ed5183877e52404284f
Eric Nelson II <nelsoneric2011@gmail.com>
2018-12-01 18:16:12 -0800

    functional Liri app

--- keys.js
+++ keys.js
@@ -1,9 +1,10 @@
-var keys = require("./keys.js");
-console.log('this is loaded');
+// Load Spotify npm package
+var spotify = require('spotify');
+// console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
 
-var Spotify = new Spotify(keys.spotify);
+
--- liri.js
+++ liri.js
@@ -1,22 +1,21 @@
 // Requirements
 require("dotenv").config();
+var axios = require("axios")
 var moment = require('moment');
 
 // Spotify requirements
-// var keys = require("./keys.js");
-// var Spotify = require('node-spotify-api');
-// var spotify = new Spotify({
-//     id: keys.spotify.id,
-//     secret: keys.spotify.secret
-// });
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
 
 // liri node commands 
 var command = process.argv[2];
 var query = process.argv[3];
 // Movie request
 var movieThis = function (movieQuery) {
-    // npm module
-    var request = require("request");
+    var divider = "\n------------------------------------------------------------\n\n";
+    console.log(divider);
 
     // If user doesnt select, default to Mr. Robot
     if (movieQuery === undefined) {
@@ -24,89 +23,103 @@ var movieThis = function (movieQuery) {
     }
 
     // GET request
-    request("http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            console.log(" Title: " + JSON.parse(body).Title);
-            console.log(" Year: " + JSON.parse(body).Year);
-            console.log(" IMDB Rating: " + JSON.parse(body).imdbRating);
-            console.log(" Country: " + JSON.parse(body).Country);
-            console.log(" Language: " + JSON.parse(body).Language);
-            console.log(" Plot: " + JSON.parse(body).Plot);
-            console.log(" Actors: " + JSON.parse(body).Actors);
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).value);
-            // create a for loop to look though object to find Rotten Tomatoes rating
-            // if the Rotten Tomatoes rating is found, print widow 
-            // for (var i = 0; JSON.parse(body).Ratings.length; i++) {
-            //     
+    var URL = "http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&apikey=trilogy";
 
-            // }
-        }
+    axios.get(URL).then(function (response) {
+        var movieData = response.data;
+
+        var showData = [
+            "Title: " + movieData.Title,
+            "Year: " + movieData.Year,
+            "IMDB Rating: " + movieData.Rated,
+            "Rotten Tomatoes Rating: " + movieData.Ratings[1].Value,
+            "Country: " + movieData.Country,
+            "Language: " + movieData.Language,
+            "Plot: " + movieData.Plot,
+            "Actors: " + movieData.Actors
+        ].join("\n\n")
+        console.log(showData);
     })
 }
 
-// // Spotify request
-// var spotifyThis = function (songName) {
-//     // spotify package
-//     var spotify = require("node-spotify-api");
+// Spotify This
+var spotifyThis = function () {
+    var divider = "\n------------------------------------------------------------\n\n";
+    console.log(divider);
+    spotify.search({ type: 'track', query }, function (err, response, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var showData = [
+            "Artist Name: " + response.tracks.items[0].artists[0].name,
+            "Song Name: " + response.tracks.items[0].name,
+            "Album Name: " + response.tracks.items[0].album.name
+        ].join("\n\n")
 
-//     // if user doesnt make a selection default, "The Sign"
-//     if (songName  === undefined) {
-//         songName = "The Sign By Ace of Base"
-//     }
-
-//     // Spotitfy API request, if a object is fond then it will return the first artist, song, preview link and album
-//     spotify.search({type: 'track', query: songName, limit: 10}, function (err, data) {
-//         if (error) {
-//             return console.log("ERROR: " + err);
-//         } else {
-
-//             // create a for loop for when a song has multiple artists
-//             for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
-//                 if (i === 0) {
-//                     console.log("Artist: " + data.track.items[0].artists[i].name);
-//                 } else {
-//                     console.log("  " + data.tracks.items[0].artists[i].name)
-//                 }
-//             }
-
-//             console.log("Song:         " + data.tracks.items[0].name);
-//             console.log("Preview Link: " + data.tracks.items[0].preview_url);
-//             console.log("Album:        " + data.tracks.items[0].album.name);
-//         }
-//     });
-// }
+        if (query === undefined) {
+            query === "The Sign by Ace of Base";
+        }
+        console.log(showData);
+        // console.log(response.tracks.items[0].artists);
+    });
+};
 
 // Concert thus 
 var concertThis = function (artistQuery) {
-    // npm module
-    var request = require("request");
-
-    // If user doesnt select, default to Mr. Robot
-    if (artistQuery === undefined) {
-        artistQuery = "Kanye West";
-    }
+    var divider = "\n------------------------------------------------------------\n\n";
+    console.log(divider);
 
     // GET request
-    request("https://rest.bandsintown.com/artists/" + artistQuery + "/events?app_id=codingbootcamp", function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            // test
-            // console.log(response);
-            var result = JSON.parse(body)[0];
-            console.log(" Venue Name: " + result.venue.name);
-            console.log(" Venue Location: " + result.venue.city);
-            console.log(" Event Date: " + moment(result.date).format("MM/DD/YYYY"));
+    var URL = "https://rest.bandsintown.com/artists/" + artistQuery + "/events?app_id=codingbootcamp";
+    axios.get(URL).then(function (response) {
+        // If user doesnt select, default to Mr. Robot
+        if (artistQuery === undefined) {
+            artistQuery = "Kanye West";
+            console.log("Since you counldn't choose an concert, I picked Kanye West for you.")
         }
+        var concertData = response.data[0].venue;
+        var showData = [
+            "Venue: " + concertData.name,
+            "City: " + concertData.city,
+            "date: " + moment(response.data[0].datetime).format("MM/DD/YYYY")
+        ].join("\n\n")
+        console.log(showData);
     })
+
+
 }
 // app functionality by user input 
 if (command === "movie-this") {
     movieThis(query);
-// } else if (command === "spotify-this-song") {
-//     spotifyThis(query);
+} else if (command === "spotify-this-song") {
+    spotifyThis(query);
 } else if (command === "concert-this") {
     concertThis(query);
 } else if (command === "do-what-it-says") {
     var fs = require("fs")
-    
 
+    fs.readFile("random.txt", "utf-8", function (error, data) {
+        var command;
+        var query;
+
+        if (data.indexOf(",") !== -1) {
+            var dataArr = data.split(",");
+            command = dataArr[0];
+            query = dataArr[1];
+        } else {
+            command = data;
+        }
+
+        if (command == "movie-this") {
+            movieThis();
+        } else if (command === "spotify-song-this") {
+            spotifyThis(query);
+        } else if (command === "concert-this") {
+            concertThis(query);
+        } 
+    });
+} else if (command === undefined) {
+    console.log("Please eneter a command to start using LIRI.");
+} else {
+    console.log("Command not recognized, please try again.")
 }
